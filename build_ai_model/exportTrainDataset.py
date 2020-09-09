e07b3f90d1ae8ce83720504e92b2997509280067
armel Ngassa <armel.g.ngassa@studmail.w-hs.de>
2020-09-09 19:58:42 +0200

    implentation of new methods

--- build_ai_model/exportTrainDataset.py
+++ build_ai_model/exportTrainDataset.py
@@ -11,13 +11,12 @@ def export():
                                  db='WordPress',
                                  charset='utf8mb4',
                                  port=3306)
-                                 #cursorclass=pymysql.cursors.DictCursor)
+    # cursorclass=pymysql.cursors.DictCursor)
 
-    #cursor = connection.cursor()
-
+    # cursor = connection.cursor()
 
     # Selecting.
-    #query = "SELECT `id` FROM `table` ORDER BY RAND() LIMIT 1"
+    # query = "SELECT `id` FROM `table` ORDER BY RAND() LIMIT 1"
     """query = "SELECT `wp_ifiS_02session.session_id` AS session_id, wp_ifiS_02session.ip_address AS ip_address,
     wp_ifiS_02session.login_attempt AS login_attempt, wp_ifiS_02session.attempt_date AS attempt_date,
     wp_ifiS_02session.countrycode AS countrycode, wp_ifiS_02session.state AS state,
@@ -32,21 +31,23 @@ def export():
     AND
     wp_ifiS_02session.login_attempt = wp_ifiS_02user_recognition.login_attempt"""
 
-    #cursor.execute(query)
+    # cursor.execute(query)
 
-    #if cursor.rowcount == 0:
+    # if cursor.rowcount == 0:
     #    print('No results matched your query.')
-    #else:
+    # else:
     #    print(cursor.fetchone()['id'])
 
     # Inserting
-    #query = "INSERT INTO `table` (id) VALUES (1)"
-    #cursor.execute(query)
+    # query = "INSERT INTO `table` (id) VALUES (1)"
+    # cursor.execute(query)
 
     connection.commit()  # You need this if you want your changes 'commited' to the database.
 
 
 def exportDBUBAIFIS():
+    start_time = time.time()
+
     try:
         connection = mysql.connector.connect(host='194.94.127.112',
                                              database='WordPress',
@@ -62,30 +63,54 @@ def exportDBUBAIFIS():
 
             mycursor = connection.cursor()
 
-            mycursor.execute('SELECT wp_ifiS_02session.session_id AS session_id, wp_ifiS_02session.ip_address AS ip_address, '
-                             'wp_ifiS_02session.login_attempt AS login_attempt, wp_ifiS_02session.attempt_date AS attempt_date, ' \
-                             'wp_ifiS_02session.countrycode AS countrycode, wp_ifiS_02session.state AS state,' \
-                            'wp_ifiS_02user_recognition.browser AS browser, wp_ifiS_02user_recognition.browser_version AS browser_version,' \
-                            'wp_ifiS_02user_recognition.IP AS IP, wp_ifiS_02user_recognition.user_agent AS user_agent,' \
-                            'wp_ifiS_02user_recognition.platform AS platform, wp_ifiS_02user_recognition.login_attempt AS login_attempt,' \
-                            'wp_ifiS_02user_recognition.login_date AS login_date, wp_ifiS_02user_recognition.logout_date AS logout_date,' \
-                            'wp_ifiS_02user_recognition.duration AS duration, wp_ifiS_02user_recognition.loginstatus AS loginstatus,' \
-                            'wp_ifiS_02user_recognition.subpage AS subpage FROM wp_ifiS_02session JOIN wp_ifiS_02user_recognition' \
-                            'ON wp_ifiS_02session.user_id = wp_ifiS_02user_recognition.user_id')
-                            #'ON wp_ifiS_02session.login_attempt = wp_ifiS_02user_recognition.login_attempt')
+            '''mycursor.execute('SELECT wp_ifiS_02session.session_id AS session_id, wp_ifiS_02session.ip_address AS ip_address, '
+                             'wp_ifiS_02session.login_attempt AS login_attempt, wp_ifiS_02session.attempt_date AS attempt_date, ' 
+                             'wp_ifiS_02session.countrycode AS countrycode, wp_ifiS_02session.state AS state,' 
+                            'wp_ifiS_02user_recognition.browser AS browser, wp_ifiS_02user_recognition.browser_version AS browser_version,' 
+                            'wp_ifiS_02user_recognition.IP AS IP, wp_ifiS_02user_recognition.user_agent AS user_agent,' 
+                            'wp_ifiS_02user_recognition.platform AS platform, wp_ifiS_02user_recognition.login_attempt AS login_attempt,' 
+                            'wp_ifiS_02user_recognition.login_date AS login_date, wp_ifiS_02user_recognition.logout_date AS logout_date,' 
+                            'wp_ifiS_02user_recognition.duration AS duration, wp_ifiS_02user_recognition.loginstatus AS loginstatus,' 
+                            'wp_ifiS_02user_recognition.subpage AS subpage FROM wp_ifiS_02session JOIN wp_ifiS_02user_recognition,'
+                            'ON wp_ifiS_02session.user_id = wp_ifiS_02user_recognition.user_id,'
+                            'ON wp_ifiS_02session.login_attempt = wp_ifiS_02user_recognition.login_attempt') '''
+            mycursor.execute('SELECT * FROM `wp_ifiS_02user_recognition` ORDER BY `id` ASC')
+            userRecoTab = mycursor.fetchall()
+            print(userRecoTab)
 
-            result = mycursor.fetchall()
+        def fetchUserRecoTab():
+            mycursor.execute('SELECT * FROM `wp_ifiS_02user_recognition` ORDER BY `id` ASC')
+            userRecoTab = mycursor.fetchall()
+            print(userRecoTab)
 
-            for i in result:
-                print(i)
+        def fetchUserSessionTab():
+            mycursor.execute('SELECT * FROM `wp_ifiS_02session` ORDER BY `id` ASC')
+            userSessionTab = mycursor.fetchall()
+            print(userSessionTab)
+
+        # userReco = fetchUserRecoTab()
+        # userSession = fetchUserSessionTab()
+        # print(userReco)
+        # print(userSession)
+        # merged = []
+        # for user in userReco[1]:
+        #    uid = user
+        #    uid += (['', ''])  # ip and user_id
+        #    for session in userSession[1]:
+        #        if uid[0] == session[1]:
+        #            uid[3] = session[2]
+        #            break
+        #    merged.append(uid)
+        # return merged
 
     except Error as e:
         print("Error while connecting to MySQL", e)
     finally:
-        if (connection.is_connected()):
+        if connection.is_connected():
             cursor.close()
             connection.close()
             print("MySQL connection is closed")
+    print("--- %s seconds ---" % (time.time() - start_time))
 
 
 def exportedDB():
@@ -106,7 +131,6 @@ def exportedDB():
 
 
 def exportDB():
-
     mydbconnect = mysql.connector.connect(host="194.94.127.112", user="ifis1",
                                           passwd="b0QrDr8ShG#e@iMWDwGKlgw3", database="WordPress")
 
@@ -120,12 +144,32 @@ def exportDB():
         print(i)
 
 
+def something(duration=0.000001):
+    """
+    Function that needs some serious benchmarking.
+    """
+    time.sleep(duration)
+    # You may return anything you want, like the result of a computation
+    return 123
+
+
+def test_my_stuff(benchmark):
+    # benchmark something
+    result = benchmark(something)
+
+    # Extra code, to verify that the run completed correctly.
+    # Sometimes you may want to check the result, fast functions
+    # are no good if they return incorrect results :-)
+    assert result == 123
+
+
 if __name__ == '__main__':
+    import time
     import pymysql
     import mysql.connector
     from mysql.connector import Error
     import sqlparse
 
-    #exportdb()
-    #exportedDB()
+    # exportdb()
+    # exportedDB()
     exportDBUBAIFIS()
