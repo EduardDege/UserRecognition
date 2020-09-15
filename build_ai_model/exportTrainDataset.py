956de578c04a5c0e046c85ba755c1bc674ef0451
armel Ngassa <armel.g.ngassa@studmail.w-hs.de>
2020-09-15 15:55:27 +0200

    add a new function for preprocessing the Data and a function to build a model

--- build_ai_model/exportTrainDataset.py
+++ build_ai_model/exportTrainDataset.py
@@ -2,9 +2,10 @@
  This is the Tool to analyse a Use Behavior on a Wordpress-Website
  The User can be blocker, if the Machine Learning Algo evaluate the use ask risk
 """
+from builtins import print
 
 
-def mariadbtest():
+def exportDatasets():
     start_time = time.time()
     pool = mariadb.ConnectionPool(
         pool_name='pool1',
@@ -24,30 +25,43 @@ def mariadbtest():
 
         cursor = conn1.cursor()
         cursor.execute('SELECT wp.session_id, wp.ip_address, wp.login_attempt, '
-                   'wp.countrycode, wp.user_id, wp.state FROM wp_ifiS_02session wp')
+                       'wp.countrycode, wp.user_id, wp.state FROM wp_ifiS_02session wp')
 
         result_session = cursor
 
         cursor = conn2.cursor()
         cursor.execute('SELECT ur.user_id, ur.browser, ur.browser_version, ur.user_agent, ur.platform, '
-                   'ur.login_attempt, ur.login_date, ur.logout_date, ur.duration, ur.loginstatus, ur.subpage'
-                   ' FROM wp_ifiS_02user_recognition ur')
+                       'ur.login_attempt, ur.login_date, ur.logout_date, ur.duration, ur.loginstatus, ur.subpage'
+                       ' FROM wp_ifiS_02user_recognition ur')
 
         result_ur = cursor
 
         df_session = DataFrame(result_session, columns=["session_id", "ip_address", "login_attempt", "countrycode",
-                                                    "user_id", "state"])
+                                                        "user_id", "state"])
         df_ur = DataFrame(result_ur, columns=["user_id", "browser", "browser_version", "user_agent", "platform",
-                                          "login_attempt", "login_date", "logout_date", "duration",
-                                          "loginstatus", "subpage"])
+                                              "login_attempt", "login_date", "logout_date", "duration",
+                                              "loginstatus", "subpage"])
 
-        merged = pd.merge(df_session, df_ur, how='inner', left_on='user_id', right_on='user_id')
-        frames = [df_session, df_ur]
+        dataset = pd.merge(df_session, df_ur, how='inner', left_on='user_id', right_on='user_id')
+        print(dataset.tail())
+        #
+        X = dataset.values
+        y = dataset.values
 
-        result = pd.concat(frames)
+        print(X)
 
-        print(merged)
-        print(result)
+        onehotencoder = OneHotEncoder(categorical_features=[1])
+        X = onehotencoder.fit_transform(X).toarray()
+        X = X[:, 0:]
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+        # Feature Scaling
+        sc = StandardScaler()
+        X_train = sc.fit_transform(X_train)
+        X_test = sc.fit_transform(X_test)
+        print(X_train)
+        # print(result)
         conn1.close()
         conn2.close()
 
@@ -55,6 +69,37 @@ def mariadbtest():
 
     except mariadb.Error as e:
         print(f"Error:{e}")
+
+
+def preprocess():
+    # Store the variable we'll be predicting on.
+    target = "Rentalcount"
+
+    # Generate the training set.  Set random_state to be able to replicate results.
+    train = df.sample(frac=0.8, random_state=1)
+
+    # Select anything not in the training set and put it in the testing set.
+    test = df.loc[~df.index.isin(train.index)]
+
+    # Print the shapes of both sets.
+    print("Training set shape:", train.shape)
+    print("Testing set shape:", test.shape)
+
+    # Initialize the model class.
+    lin_model = LinearRegression()
+
+    # Fit the model to the training data.
+    lin_model.fit(train[columns], train[target])
+
+
+def build_model():
+    model = Sequential()
+    model.add(Dense(output_dim=6, init='uniform', activation='relu',
+                    input_dim=11))  # input_dim = number of input variables
+    model.add(Dense(output_dim=6, init='uniform', activation='relu'))
+    model.add(Dense(output_dim=1, init='uniform', activation='sigmoid'))
+    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    return model
 
 
 def exportDBUBAIFIS():
@@ -138,20 +183,24 @@ def exportDBUBAIFIS():
                                                   "login_attempt", "login_date", "logout_date", "duration",
                                                   "loginstatus", "subpage"])
 
-            merged = pd.merge(df_session, df_ur, how='inner', left_on='user_id', right_on='user_id')
+            dataset = pd.merge(df_session, df_ur, how='inner', left_on='user_id', right_on='user_id')
 
-            frames = [df_session, df_ur]
+            X = dataset.iloc[:, 3:13].valu
+            y = dataset.iloc[:, -1].values
+            #print(X)
+            #print(dataset)
+            # frames = [df_session, df_ur]
 
-            result = pd.concat(frames)
+            # result = pd.concat(frames)
 
             # pd.set_option("display.max_rows", None)
             # pd.set_option("display.max_columns", None)
             # pd.set_option("display.width", None)
 
-            print(merged)
+
             # print(result)
-            print(len(result_session))
-            print(len(result_ur))
+            # print(len(result_session))
+            # print(len(result_ur))
             print(time.time() - start_time)
 
     except Error as e:
@@ -171,6 +220,9 @@ if __name__ == '__main__':
     import pandas as pd
     from pandas import DataFrame
     import mariadb
+    import numpy
+    from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+    from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
 
     # exportDBUBAIFIS()
-    mariadbtest()
+    exportDatasets()
